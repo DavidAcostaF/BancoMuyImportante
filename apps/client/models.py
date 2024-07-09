ed6c471586ad7087657bb82738b34f9bd4fba43d
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2024-07-09 15:39:47 -0700

    settings

--- apps/client/models.py
+++ apps/client/models.py
@@ -13,21 +13,4 @@ class Client(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.middle_name}"
 
-    def deposit(self, amount):
-        self.balance += amount
-        self.save()
 
-    def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
-            self.save()
-        else:
-            raise ValueError("Insufficient balance")
-
-    def transfer(self, amount, destination_account:'Client'):
-        if self.balance >= amount:
-            self.balance -= amount
-            destination_account.deposit(amount)
-            self.save()
-        else:
-            raise ValueError("Insufficient balance")
--- apps/client/views.py
+++ apps/client/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+"""
+    def deposit(self, amount):
+        self.balance += amount
+        self.save()
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            self.save()
+        else:
+            raise ValueError("Insufficient balance")
+
+    def transfer(self, amount, destination_account:'Client'):
+        if self.balance >= amount:
+            self.balance -= amount
+            destination_account.deposit(amount)
+            self.save()
+        else:
+            raise ValueError("Insufficient balance")
+"""

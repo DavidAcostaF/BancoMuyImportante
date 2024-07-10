b5f5c569cbea7fb2fe9164db6ab71fed9ae4e264
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2024-07-10 02:41:15 -0700

    Settings finished

--- apps/client/models.py
+++ apps/client/models.py
@@ -1,6 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager,AbstractBaseUser,PermissionsMixin,AbstractUser
 
-class Client(models.Model):
+class ClientManager(BaseUserManager):
+    def _create_user(self,email,first_name,last_name,password,**extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        if not first_name:
+            raise ValueError('First name is required')
+        if not last_name:
+            raise ValueError('Last name is required')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self,email,first_name,last_name,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_active',True)
+        extra_fields.setdefault('is_superuser',False)
+        return self._create_user(email,first_name,last_name,password,**extra_fields)
+
+    def create_superuser(self,email,first_name,last_name,password,**extra_fields):
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_active',True)
+        extra_fields.setdefault('is_superuser',True)
+        return self._create_user(email,first_name,last_name,password,**extra_fields)
+    
+
+class Client(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50)
@@ -9,8 +41,12 @@ class Client(models.Model):
     password = models.CharField(max_length=50)
     address = models.CharField(max_length=255)
     balance = models.DecimalField(max_digits=15, decimal_places=2, default=0.00)
+    email = models.EmailField(unique=True)
+    objects = ClientManager()
 
+    REQUIRED_FIELDS = ['first_name','last_name','email']
+    USERNAME_FIELD = 'username'
+
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.middle_name}"
-
-
